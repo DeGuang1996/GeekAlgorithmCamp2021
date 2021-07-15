d7f7959047c8b22cea7c52a4a09840ba3baf0594
deguang <1969117214@qq.com>
2021-07-16 03:29:06 +0800

    第四周作业提交

---
+++ src/main/java/week04/FindMin.java
@@ -0,0 +1,21 @@
+package week04;
+
+public class FindMin {
+
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            // 是否有重复元素
+            // } else {
+            //     right--;
+            // }
+        }
+        return nums[left];
+    }
+}
